8709e4ffb187267b35295d3256bafcd26c96d330
Antonio <gantoniodeveloper@otulook.com>
2020-07-11 16:40:40 -0500

    altertable

--- database/migrations/1594503560833_user_schema.js
+++ database/migrations/1594503560833_user_schema.js
@@ -3,19 +3,19 @@
 /** @type {import('@adonisjs/lucid/src/Schema')} */
 const Schema = use('Schema')
 
-class AlterUserSchema extends Schema {
+class UserSchema extends Schema {
   up () {
-    this.table('alter_users', (table) => {
+    this.table('users', (table) => {
       table.date('edad',8).nullable()
       // alter table
     })
   }
 
   down () {
-    this.table('alter_users', (table) => {
+    this.table('users', (table) => {
       // reverse alternations
     })
   }
 }
 
-module.exports = AlterUserSchema
+module.exports = UserSchema
