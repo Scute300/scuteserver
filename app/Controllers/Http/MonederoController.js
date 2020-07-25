2a1b989d17c363883fe5084cd2c9e935bd2cb726
antonio <gantoniodeveloper@outlook.com>
2020-07-24 23:12:13 -0500

    ttt

--- app/Controllers/Http/MonederoController.js
+++ app/Controllers/Http/MonederoController.js
@@ -3,10 +3,17 @@ const Monedero = use ('App/Models/Monedero')
 
 class MonederoController {
     async obtain({auth, response}){
+        try{
             const user = auth.current.user
-            const monedero = await Monedero.findByOrFail('user_id', user.id)
+            const monedero = await Monedero.query()
+            .where('user_id', user.id)
             console.log(monedero)
+
             return response.json('Hola mundo')
+        }catch(error){
+            console.log(error)
+            return response.status(400).json('hola mundo')
+        }
     }
 }
 
