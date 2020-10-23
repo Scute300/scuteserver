4bdae25fe655f5a88534cef72f6f4759450b9872
Antonio <gantoniodeveloper@outlook.com>
2020-10-23 06:58:32 -0500

    =

--- app/Controllers/Http/BuqueController.js
+++ app/Controllers/Http/BuqueController.js
@@ -2,20 +2,25 @@
 import Buque from 'App/Models/Buque'
 class BuqueController {
     async añadirbuque({auth, request, response}){
-        const data = request.only(['nombrebuque', 'empresa', 'eslora', 'potencia', 'manga', 'DWT', 'puntal'])
-        const buque = new Buque()
-        buque.usert_id = auth.current.user.id
-        buque.empresa = data.empresa
-        buque.eslora = data.eslora
-        buque.potencia = data.potencia
-        buque.manga= data.manga
-        buque.DWT = data.DWT
-        buque.puntal = data.puntal
-        await buque.save()
 
-        return response.status(400).json({
-            status: 'sure'
-        })
+        try{
+            const data = request.only(['nombrebuque', 'empresa', 'eslora', 'potencia', 'manga', 'dwt', 'puntal'])
+            const buque = new Buque()
+            buque.usert_id = auth.current.user.id
+            buque.empresa = data.empresa
+            buque.eslora = data.eslora
+            buque.potencia = data.potencia
+            buque.manga= data.manga
+            buque.DWT = data.dwt
+            buque.puntal = data.puntal
+            await buque.save()
+    
+            return response.json({
+                status: 'sure'
+            })
+        } catch(error){
+            console.log(error)
+        }
 
     }
 }
