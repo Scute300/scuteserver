d48f68097b82a2fe6b8e8e3e1644d1c4dc592202
Antonio <gantoniodeveloper@outlook.com>
2020-10-25 16:07:37 -0600

    eliminarusuarios

--- app/Controllers/Http/BuqueuserController.js
+++ app/Controllers/Http/BuqueuserController.js
@@ -35,6 +35,49 @@ class BuqueuserController {
             data: userbuques
         })
     }
+
+    async obtenerunusuario ({params, response}){
+        const usuario = await Buqueuser.findBy('id',params.id)
+
+        return response.json({
+            data: usuario
+        })
+    }
+
+    async eliminarusuario ({params, response}){
+        const usuario = await Buqueuser.findBy('id',params.id)
+        await usuario.delete()
+        return response.json({
+            data: 'eliminado'
+        })
+    }
+
+    async editarusuario({auth, request,response, params}){
+        const data = request.only(['username', 'name', 'apellido', 'email', 'cargo', 'rol', 'doublecheck', 'buques'])
+        const user= await Buqueuser.findBy('id',params.id)
+        user.username = data.username
+        user.name = data.name 
+        user.apellido = data.apellido
+        user.email = data.email
+        user.cargo = data.cargo
+        user.rol = data.rol
+        user.doublecheck = data.doublecheck
+        await user.save()
+
+        if(data.buques.length !== 0){
+            for(let i = 0; i < data.buques.length; i++){
+                const cargo = await new CargoBuque()
+                cargo.buqueuser_id = user.id
+                cargo.buque = data.buques[i]
+                await cargo.save()
+            }
+        }
+
+        return response.json({
+            data: 'sure'
+        })
+
+    }
 }
 
 module.exports = BuqueuserController
