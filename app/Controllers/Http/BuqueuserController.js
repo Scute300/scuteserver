6dde721d99c10f1d55f3e3e65e134d0751ebee56
Antonio <gantoniodeveloper@outlook.com>
2020-10-25 14:11:11 -0600

    ===

--- app/Controllers/Http/BuqueuserController.js
+++ app/Controllers/Http/BuqueuserController.js
@@ -3,7 +3,7 @@ const Buqueuser = use('App/Models/Buqueuser')
 const CargoBuque = use('App/Models/Cargobuque')
 class BuqueuserController {
     async añadirusuario({auth, request, response}){
-        const data = request.only(['username', 'name', 'apellido', 'email', 'cargo', 'rol', 'check', 'buques'])
+        const data = request.only(['username', 'name', 'apellido', 'email', 'cargo', 'rol', 'doublecheck', 'buques'])
         const user = await new Buqueuser()
         user.username = data.username
         user.name = data.name 
@@ -11,7 +11,7 @@ class BuqueuserController {
         user.email = data.email
         user.cargo = data.cargo
         user.rol = data.rol
-        user.check = data.check
+        user.doublecheck = data.check
         await user.save()
 
         if(data.buques.length !== 0){
